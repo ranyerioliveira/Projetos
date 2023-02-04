12207dc005ba89d11755448e4790741e2446f238
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-04 11:43:17 -0300

    atualização
    at

--- FormulariosDePesquisas/formulario.js
+++ FormulariosDePesquisas/formulario.js
@@ -1,2 +1,2 @@
-var pagina = window.document.getElementById('atualizar')
-pagina.window.location.href = pagina.window.location.href
+var pagina = window.document.getElementById('atualizar');
+pagina.window.location.href = window.location.href;
